c6bcc790159bb3ff161db1049ce77fbe5d33e0ae
Peter <50802550+peterfoo22@users.noreply.github.com>
2024-10-03 05:38:21 -0400

    updated with actions

---
+++ src/actions/index.ts
@@ -0,0 +1,44 @@
+'use server';
+
+import { redirect } from 'next/navigation';
+import { db } from '@/db';
+
+export async function editSnippet(id: number, code: string) {
+  await db.snippet.update({
+    where: { id },
+    data: { code },
+  });
+
+  redirect(`/snippets/${id}`);
+}
+
+export async function deleteSnippet(id: number) {
+  await db.snippet.delete({
+    where: { id },
+  });
+
+  redirect('/');
+}
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  return {
+    message: 'Title must be longer',
+  };
+  // // Check the user's inputs and make sure they're valid
+  // const title = formData.get('title') as string;
+  // const code = formData.get('code') as string;
+
+  // // Create a new record in the database
+  // const snippet = await db.snippet.create({
+  //   data: {
+  //     title,
+  //     code,
+  //   },
+  // });
+
+  // // Redirect the user back to the root route
+  // redirect('/');
+}
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,25 +1,11 @@
 import { db } from "@/db"
 import { redirect } from "next/navigation"
+import * as actions from "@/actions"
+
+
 
 export default function SnippetCreatePage(){
 
-  async function createSnippet(formData: FormData) {
-    
-    'use server';
-
-    const title = formData.get('title') as string;
-    const code = formData.get('code') as string;
-
-    const snippet = await db.snippet.create({
-      data: {
-        title: title,
-        code: code
-      },
-    });
-
-    console.log(snippet);
-    redirect(`/`);
-  }
 
   return (
     <form action={createSnippet}>
