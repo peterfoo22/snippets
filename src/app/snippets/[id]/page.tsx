7c7f393bf0b55bf0d0d6b8097ab801ae2c7e3afd
Peter Suh <psuh22@gmail.com>
2024-10-05 17:19:35 +0200

    updated

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -45,4 +45,10 @@ export default async function SnippetPage(props: SnippetShowProps) {
   );
 } 
 
+export async function generateStaticParams() {
+  const snippets = await db.snippet.findMany();
+  return snippets.map((snippet) => ({
+    id: snippet.id.toString(),
+  }));
+}
 //testing testing
